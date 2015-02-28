c445d835a6051fd1dc612c9dc8a6f8eaa7c3b367
Takemi Watanuki <watakemi725@Takemi-no-MacBook-Pro.local>
2015-02-28 09:16:34 +0900

    hello

---
+++ Sample0228Tests/Sample0228Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Sample0228Tests.swift
+//  Sample0228Tests
+//
+//  Created by Takemi Watanuki on 2015/02/28.
+//  Copyright (c) 2015年 watakemi725. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sample0228Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
